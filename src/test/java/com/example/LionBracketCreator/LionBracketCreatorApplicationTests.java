4cdb240cecf561e244a7374546f756e588def001
Peter Yuan <peter.yuan18@gmail.com>
2025-05-19 16:13:58 -0400

    Download project from spring.start.io

---
+++ src/test/java/com/example/LionBracketCreator/LionBracketCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LionBracketCreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LionBracketCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
