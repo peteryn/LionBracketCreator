54b84e9b4b2eec1dfede30c56bf1a4605cb76272
Peter Yuan <peter.yuan18@gmail.com>
2025-05-19 17:48:34 -0400

    Setup application.yml for oauth login

--- src/main/java/com/example/LionBracketCreator/LionBracketCreatorApplication.java
+++ src/main/java/com/example/LionBracketCreator/LionBracketCreatorApplication.java
@@ -3,10 +3,14 @@ package com.example.LionBracketCreator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class LionBracketCreatorApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(LionBracketCreatorApplication.class, args);
 	}
 
