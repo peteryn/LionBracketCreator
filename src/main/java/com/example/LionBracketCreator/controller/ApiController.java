3a71396eab79c6bf57ddf7dbe618bdfe1c5498d8
peteryn <peter.yuan18@gmail.com>
2025-06-25 13:11:50 -0400

    Remove import

--- src/main/java/com/example/LionBracketCreator/controller/ApiController.java
+++ src/main/java/com/example/LionBracketCreator/controller/ApiController.java
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Optional;
 
 @RestController
